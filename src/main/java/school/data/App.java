557c56a0bd01119cc509b4baa356877b3e576a78
Creaturelove7 <rrz@bu.edu>
2023-09-26 06:46:54 -0400

    finish assign2 5 question!

--- src/main/java/school/data/App.java
+++ src/main/java/school/data/App.java
@@ -1,72 +1,146 @@
 package school.data;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Map;
 
 public class App 
 {
+	private static final Map<String, LocalDateTime> memory = new HashMap<>();
+	 private static final Map<String, Integer> keywordCount = new HashMap<>(); // For counting the frequency
+
 	public static void main(String[] args) {
-		// Define the name of output
-        String outputFile = "merged.json";
-        // Define the name of input
-        String[] inputFiles = {"1.json", "2.json", "3.json"};
-
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))) {
-            writer.write("["); // the start [
-
-            boolean first = true;
-
-            for (String inputFile : inputFiles) {
-                try (BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
-                    // Read every line and add it in to new file
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        if (!first) {
-                            writer.write(","); // add ","
-                        } else {
-                            first = false;
-                        }
-                        writer.newLine(); // Add new line
-                        writer.write(line);
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            writer.newLine();
-            writer.write("]"); // The end ]
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//		// Define the name of output
+//        String outputFile = "merged.json";
+//        // Define the name of input
+//        String[] inputFiles = {"D:\\Database\\1.json", "D:\\Database\\2.json", "D:\\Database\\3.json"};
+//
+//        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))) {
+//            writer.write("["); // the start [
+//
+//            boolean first = true;
+//
+//            for (String inputFile : inputFiles) {
+//                try (BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
+//                    // Read every line and add it in to new file
+//                    String line;
+//                    while ((line = reader.readLine()) != null) {
+//                        if (!first) {
+//                            writer.write(","); // add ","
+//                        } else {
+//                            first = false;
+//                        }
+//                        writer.newLine(); // Add new line
+//                        writer.write(line);
+//                    }
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//
+//            writer.newLine();
+//            writer.write("]"); // The end ]
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
         
-     // The key to search for in the JSON file
-        String targetKey = "name";
-        // The name of the JSON file to search
-        String fileName = "merged.json";
 
-        // Open the file for reading
-        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-            String line;
+		
+		
+       //Quick and comprehensive output version with Jackson
+		File jsonFile = new File("D:\\Database\\merged.json");
+        ArrayList<String> input = new ArrayList<>();
+        input.add("creator");
+        input.add("baseball");
+		for(String keyword: input) {
+			//Add key and time stamp to memory
+			storeSearchKeyword(keyword);
+			try {
+	            JsonFactory factory = new JsonFactory();
+	            JsonParser parser = factory.createParser(jsonFile);
 
-            // Read the file line by line
-            while ((line = reader.readLine()) != null) {
-                // Search for the key in the current line and print its value
-                int index = line.indexOf("\"" + targetKey + "\"");
-                if (index != -1) {
-                    int colonIndex = line.indexOf(":", index);
-                    int commaIndex = line.indexOf(",", colonIndex);
-                    int endIndex = (commaIndex != -1) ? commaIndex : line.indexOf("}", colonIndex);
-                    if (colonIndex != -1 && endIndex != -1) {
-                        String value = line.substring(colonIndex + 1, endIndex).trim();
-                        System.out.println(targetKey + ": " + value);
-                    }
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+	            String name = "";
+	            String category = "";
+	            int fundingAmount = 0;
+	            boolean isMatched = false;
+
+	            while (!parser.isClosed()) {
+	            	
+	                JsonToken jsonToken = parser.nextToken();
+
+	                if (jsonToken == null) break;
+
+	                if (JsonToken.FIELD_NAME.equals(jsonToken)) {
+	                    String fieldName = parser.getCurrentName();
+	                    jsonToken = parser.nextToken();
+
+	                    switch (fieldName) {
+	                        case "name":
+	                            name = parser.getText();
+	                            if (name.contains(keyword)) isMatched = true;
+	                            break;
+	                        case "category":
+	                            category = parser.getText();
+	                            if (category.contains(keyword)) isMatched = true;
+	                            break;
+	                        case "fundingAmount":
+	                            fundingAmount = parser.getIntValue();
+	                            break;
+	                    }
+	                }
+
+	                if (jsonToken.isStructEnd()) {
+	                    if (isMatched) {
+	                    	
+	                        System.out.println("Project Name: " + name);
+	                        System.out.println("Category: " + category);
+	                        System.out.println("Funding Amount: " + fundingAmount);
+	                        System.out.println();
+	                        isMatched = false;
+	                    }
+	                    name = "";
+	                    category = "";
+	                    fundingAmount = 0;
+	                }
+	            }
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
         }
-        
+		// Output the memory to the console
+		outputMemory();
+		// Output the keyword search frequency
+        outputKeywordFrequency();
 	}
+	//Store memory function
+	 private static void storeSearchKeyword(String keyword) {
+	        memory.put(keyword, LocalDateTime.now());
+	        keywordCount.put(keyword, keywordCount.getOrDefault(keyword, 0) + 1); // update the count
+	    }
+	 //Output memory function
+	 private static void outputMemory() {
+	        for (Map.Entry<String, LocalDateTime> entry : memory.entrySet()) {
+	            System.out.println("Keyword: " + entry.getKey() + ", Timestamp: " + entry.getValue());
+	        }
+	    }
+	// New method to output keyword frequency
+	    private static void outputKeywordFrequency() {
+	        for (Map.Entry<String, Integer> entry : keywordCount.entrySet()) {
+	            System.out.println("Keyword: " + entry.getKey() + ", Searched: " + entry.getValue() + " times");
+	        }
+	    }
 }
 
 
